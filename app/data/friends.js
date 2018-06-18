f1770949ac8d030efb8bff8bb223f3028afaadee
Nirav Patel <nrvpatel03@gmail.com>
2018-06-17 20:01:41 -0700

    images

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,32 +1,26 @@
 
 //data ACTUAL
-// module.exports = [
-//     {
-//         name: "Person1",
-//         scores: [3, 4, 2, 1, 4, 5, 3, 4, 5, 2]
-//     },
-//     {
-//         name: "Person2",
-//         scores: [5, 2, 3, 3, 5, 2, 1, 4, 5, 2]
-//     },
-//     {
-//         name: "Person3",
-//         scores: [2, 4, 5, 2, 3, 4, 5, 3, 2, 5]
-//     },
-//     {
-//         name: "Person4",
-//         scores: [4, 3, 4, 1, 4, 5, 2, 1, 2, 3]
-//     }
-// ]
-//data TEST
 module.exports = [
     {
-        name: "testLoser",
-        scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+        name: "John",
+        scores: [3, 4, 2, 1, 4, 5, 3, 4, 5, 2],
+        image: "http://www.clker.com/cliparts/r/0/O/0/X/O/image-of-person-sspeck-md.png"
     },
     {
-        name: "testWinner",
-        scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+        name: "Mike",
+        scores: [5, 2, 3, 3, 5, 2, 1, 4, 5, 2],
+        image: "http://ramsis.co.ke/wp-content/uploads/2016/06/person.png"
+    },
+    {
+        name: "Jacob",
+        scores: [2, 4, 5, 2, 3, 4, 5, 3, 2, 5],
+        image: "http://www.relevantconnections.net/wp-content/uploads/2018/03/businessman.png"
+    },
+    {
+        name: "Hannah",
+        scores: [4, 3, 4, 1, 4, 5, 2, 1, 2, 3],
+        image: "https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/256x256/businesswoman2.png"
     }
 ]
+
 
