52c3f9eb7aacd005f2fa5c3c21becd6a16ff24e6
Nirav Patel <nrvpatel03@gmail.com>
2018-06-17 18:45:16 -0700

    logic for match

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -7,6 +7,13 @@ module.exports = function(app, data){
 
     app.post("/api/friends", function(req, res){
         var newFriend = req.body;
+        //turn scores array scores string to ints
+        for(var i = 0; i<newFriend.scores.length; i++){
+            var string = newFriend.scores[i];
+            var int = parseInt(string);
+            newFriend.scores.splice(i,1,int);
+        }
+
         console.log(newFriend);
         data.push(newFriend);
         res.json(newFriend);
